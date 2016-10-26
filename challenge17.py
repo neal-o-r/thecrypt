c82c38ea00811256ea88a0a532353a6ae0842fef
neal-o-r <neal@n-o-r.xyz>
2016-10-26 14:25:47 +0100

    17:single bit version working

--- challenge17.py
+++ challenge17.py
@@ -1,6 +1,7 @@
 import challenge12 as c12
 import challenge11 as c11
 import challenge9  as c9
+import challenge6  as c6
 import challenge10 as c10
 import challenge15 as c15
 import base64
@@ -13,13 +14,13 @@ iv  = c12.randbytes(16)
 with open('data/17.txt', 'r') as f:
         text = f.read().split('\n')
 
-plaintext = base64.b64decode(rd.choice(text[:-1]))
-
+#plaintext = base64.b64decode(rd.choice(text[:-1]))
+plaintext = bytes(range(40))
 
 def encrypt(plaintext):
 
-        plaintext = c9.PKCS7(2 * bytes(plaintext), len(key))
-
+        plaintext = c9.PKCS7(bytes(plaintext), len(key))
+        
         return c11.CBC_encrypt(plaintext, key, iv)
 
 
@@ -30,20 +31,47 @@ def padding_oracle(cyphertext):
         return c15.is_PKCS7(plaintext)
 
 
-def break_CBC(cyphertext, oracle, n):
+def break_byte(block, prev_block, known, n_byte, n):
 
-        c1 = bytes([0] * (2*n - 1))
-        c2 = cyphertext[:2*n]
+        c1_pre = bytes([0] * (n_byte - 1))
         
-
+        pad_byte = (n - n_byte) + 1
+        
+        c1_post = []
+        for i, k in enumerate(known):
+                c1_post.append(k  ^ (len(known) + 1))
+        
+        
+        c1_post = bytes(c1_post)        
+         
         for i in range(256):            
 
-                if padding_oracle(c2 + c1 + bytes([i])):
-                         c1_byte = bytearray([i])
+                c1_dash = c1_pre + bytes([i]) + c1_post
+                
+                if padding_oracle(c1_dash + block):
+                        found_byte = i
 
-        inter_byte = bytearray(c1_byte[0] ^ 1)
-        plain_byte = bytes(bytearray(cyphertext)[-1] ^ inter_byte[0])
-        print(plain_byte.decode())
+        plain_byte = bytes([prev_block[n_byte-1] ^ pad_byte ^ found_byte])
+        return plain_byte, found_byte
+
+
+
+def break_CBC(cyphertext, oracle, n):
+
+        blocks = c6.chunks(cyphertext, 16)
+
+        c1 = blocks[0]
+        c2 = blocks[1]
+
+        known = []
+        found = []
+        for i in range(2):
+                p, f = break_byte(c2, c1, found, n-i, n)
+                known.append(p)
+                found.append(f)
+
+        return known        
+        
 
 if __name__ == '__main__':
 
@@ -52,5 +80,5 @@ if __name__ == '__main__':
         cyphertext = encrypt(plaintext)
         
         decyphered = break_CBC(cyphertext, padding_oracle, n)
-
+        #print(decyphered == bytes([plaintext[2*n-1]])) 
  
