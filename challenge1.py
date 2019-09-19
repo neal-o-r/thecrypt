bcf7221b64d515063d839c4292132f13dc4395b3
Neal Ó Riain <neal.o.riain@gmail.com>
2019-09-19 16:45:08 +0100

    Update challenge1.py

--- challenge1.py
+++ challenge1.py
@@ -1,12 +1,16 @@
+#https://cryptopals.com/sets/1/challenges/1
 import base64
 
 
-def convert_hx_b64(input_hex):
+def convert_hx_b64(input_hex: str) -> bytes:
+    """
+    Take an input hexadecimal string and return it in base64
+    """
     return base64.b64encode(bytes.fromhex(input_hex))
 
 
 if __name__ == "__main__":
     with open("data/1.txt", "r") as f:
-        input_hex = f.read().splitlines()
+        input_hex, output_b64 = f.read().splitlines()
 
-    assert convert_hx_b64(input_hex[0]).decode() == input_hex[1]
+    assert convert_hx_b64(input_hex).decode() == output_b64
